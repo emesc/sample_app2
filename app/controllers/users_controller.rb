5b856f1926354f0b8aea73abee095f67af619afc
emesc <tahder77@gmail.com>
2016-10-13 00:09:45 +1100

    Finished user signup.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # redirect_to 
+      flash[:success] = "Welcome to the Sample App!"
+      # redirect_to user_url(@user)
+      redirect_to @user
     else
       render 'new'
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    # debugger
   end
 
   private
@@ -19,8 +26,4 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
-  def show
-    @user = User.find(params[:id])
-    # debugger
-  end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -6,6 +6,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # end
   test "invalid signup information" do
     get signup_path
+    assert_select 'form[action="/signup"]'
     assert_no_difference "User.count" do
       post users_path, params: { user: { name: "",
                                          email: "user@invalid",
@@ -15,5 +16,19 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
     assert_select "div#error_explanation"
     assert_select "div.field_with_errors"
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_select 'form[action="/signup"]'
+    assert_difference "User.count" do
+      post users_path, params: { user: { name: "Example User",
+                                         email: "example@railstutorial.org",
+                                         password: "foobar",
+                                         password_confirmation: "foobar" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
   end
 end
